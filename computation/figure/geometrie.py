5013bee576f93d0fe48b40f6dd8544cd12d01259
Yannis Benchaib <yannis.benchaib@gmail.com>
2026-05-12 11:49:19 +0930

    changement du système de coordonées pour que l'origine soit au centre de l'image

--- computation/figure/geometrie.py
+++ computation/figure/geometrie.py
@@ -9,21 +9,27 @@ import tkinter as tk
 import numpy as np
 
 SCALE = 2
+WIDTH = 128
+HEIGHT = 128
+WIDTH = SCALE*WIDTH
+HEIGHT = SCALE*HEIGHT
 
 #creation de la fenetre
 fenetre = tk.Tk()
-zone = tk.Canvas(fenetre)
+zone = tk.Canvas(fenetre, width=WIDTH, height=HEIGHT)
+fenetre.resizable(height=False,width=False)
 
 class ligne :
     def __init__(self,x0,y0,theta,d) :
+        self.children = []
         self.x0 = x0
         self.y0 = y0
         self.theta = theta
         self.d = d
-        self.ID = zone.create_line([self.x0*SCALE,                                  #x1
-                                    self.y0*SCALE,                                  #y1
-                                    (self.x0 + np.cos(self.theta)*self.d)*SCALE,    #x2
-                                    (self.y0 - np.sin(self.theta)*self.d)*SCALE])   #y2
+        self.ID = zone.create_line([WIDTH/2+self.x0*SCALE,                                  #x1
+                                    HEIGHT/2-self.y0*SCALE,                                 #y1
+                                    WIDTH/2+(self.x0 + np.cos(self.theta)*self.d)*SCALE,    #x2
+                                    HEIGHT/2-(self.y0 + np.sin(self.theta)*self.d)*SCALE]) #y2
         self.texte = None
     
     def delete(self) :
@@ -32,8 +38,8 @@ class ligne :
     def label(self, texte, xoff=0, yoff=0) :
         if self.texte :
             zone.delete(self.texte)
-        self.texte = zone.create_text([SCALE*(self.x0+xoff+self.d*np.cos(self.theta)/2),
-                                       SCALE*(self.y0+yoff-self.d*np.sin(self.theta)/2)],
+        self.texte = zone.create_text([WIDTH/2+SCALE*(self.x0+xoff+self.d*np.cos(self.theta)/2),
+                                       HEIGHT/2-SCALE*(self.y0+yoff+self.d*np.sin(self.theta)/2)],
                                       text = texte,
                                       anchor=tk.SW)
     
@@ -61,10 +67,10 @@ class angle :
         self.radius = radius
         self.theta0 = theta0
         self.dtheta = dtheta
-        self.ID = zone.create_arc([SCALE*(self.x0-self.radius), #x1
-                                   SCALE*(self.y0-self.radius), #y1
-                                   SCALE*(self.x0+self.radius), #x2
-                                   SCALE*(self.y0+self.radius)],#y2
+        self.ID = zone.create_arc([WIDTH/2+SCALE*(self.x0-self.radius),  #x1
+                                   HEIGHT/2-SCALE*(self.y0-self.radius), #y1
+                                   WIDTH/2+SCALE*(self.x0+self.radius),  #x2
+                                   HEIGHT/2-SCALE*(self.y0+self.radius)],#y2
                                   start=np.rad2deg(self.theta0),
                                   extent=np.rad2deg(self.dtheta),
                                   style=tk.ARC)
@@ -76,14 +82,13 @@ class angle :
     def label(self,texte,xoff=0,yoff=0) :
         if self.texte :
             zone.delete(self.texte)
-        self.texte = zone.create_text([SCALE*(self.x0+xoff+self.radius*np.cos(self.theta0+self.dtheta/2)),
-                                       SCALE*(self.y0+yoff-self.radius*np.sin(self.theta0+self.dtheta/2))],
+        self.texte = zone.create_text([WIDTH/2+SCALE*(self.x0+xoff+self.radius*np.cos(self.theta0+self.dtheta/2)),
+                                       HEIGHT/2-SCALE*(self.y0+yoff+self.radius*np.sin(self.theta0+self.dtheta/2))],
                                       text=texte,
                                       anchor=tk.SW)
 
 class ellipse :
     def __init__(self,xf,yf,a,e,theta,morceaux=20) :
-        self.children = []
         self.xf = xf
         self.yf = yf
         self.a = a
@@ -93,10 +98,9 @@ class ellipse :
         alpha = np.linspace(0, 2*np.pi,int(morceaux*SCALE))
         x0 = self.a*np.cos(alpha)-self.e*self.a
         y0 = b*np.sin(alpha)
-        x = x0*np.cos(self.theta)+np.sin(theta)*y0+xf
-        y = y0*np.cos(self.theta)-np.sin(theta)*x0+yf
+        x = WIDTH/2+SCALE*(x0*np.cos(self.theta)-y0*np.sin(theta)+self.xf)
+        y = HEIGHT/2-SCALE*(y0*np.cos(self.theta)+x0*np.sin(theta)+self.yf)
         poly = np.ravel([x,y],'F')
-        poly = poly*SCALE
         self.ID = zone.create_polygon(poly.tolist(),fill='',outline='black')
         
     def delete(self) :
@@ -104,7 +108,6 @@ class ellipse :
         
     def ligne(self, theta) :
         sortie = ligne(self.xf,self.yf,theta+self.theta,self.a*(1-self.e**2)/(1+self.e*np.cos(theta)))
-        self.children.append(sortie)
         return sortie
         
 def affichage() :
--- computation/figure/testgm.py
+++ computation/figure/testgm.py
@@ -10,7 +10,7 @@ import geometrie as gm
 #segment nommé r reliant le foyer droit avec un point 2,7 radians dans le sens direct par rapport au premier segment et un arc de cercle nommé theta
 #entre ces deux segments
 
-el = gm.ellipse(100, 75, 50, 0.9,0.3)
+el = gm.ellipse(0, 0, 50, 0.9,0.3)
 l0 = el.ligne(0)
 l1 = el.ligne(2.7)
 l0.angle(l1).label("theta")
