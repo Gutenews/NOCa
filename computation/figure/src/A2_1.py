12e37878a6670d336b7c3fbc5e293b9f59cb888b
Yannis Benchaib <yannis.benchaib@gmail.com>
2026-05-21 19:20:38 +0930

    ajout de la figure A2.1

---
+++ computation/figure/src/A2_1.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+figure A2.1
+figure pour décrire les coordonnées polaires
+"""
+import geometrie as gm
+from numpy import pi
+
+gm.configure(width=55,height=55,scale=5,fontsize=16)
+
+
+O = gm.point(-20, -20)
+O.label("O",xoff=-5,yoff=-8)
+x = gm.ligne(-20, -20, 0, 40)
+x.style('V')
+x.label("x",xoff=20)
+y = gm.ligne(-20, -20, pi/2, 40)
+y.style('V')
+y.label("y",yoff=20)
+
+r = gm.ligne(-20, -20, pi/6, 35)
+r.label("r")
+
+theta = x.angle(r)
+theta.style('V')
+theta.label("\u03B8",yoff=-5) #theta
+
+M = r.extremite()
+M.label('M',yoff=-10)
+er = gm.ligne(M.x, M.y, pi/6, 5)
+er.style('V')
+er.label("e_r",yoff=2)
+etheta = gm.ligne(M.x, M.y, 2*pi/3, 5)
+etheta.style('V')
+etheta.label("e_\u03B8",xoff=-8,yoff=3)
+
+gm.export("../out/A2_1")
