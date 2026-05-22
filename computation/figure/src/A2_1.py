a8ac61712729eba40af9787a4f888abae27a5967
Yannis Benchaib <yannis.benchaib@gmail.com>
2026-05-22 13:20:50 +0930

    modification esthétique

--- computation/figure/src/A2_1.py
+++ computation/figure/src/A2_1.py
@@ -10,7 +10,8 @@ gm.configure(width=55,height=55,scale=5,fontsize=16)
 
 
 O = gm.point(-20, -20)
-O.label("O",xoff=-5,yoff=-8)
+O.label("O",xoff=-3,yoff=-4)
+O.delete()
 x = gm.ligne(-20, -20, 0, 40)
 x.style('V')
 x.label("x",xoff=20)
@@ -19,14 +20,14 @@ y.style('V')
 y.label("y",yoff=20)
 
 r = gm.ligne(-20, -20, pi/6, 35)
-r.label("r")
+r.label("r",yoff=1)
 
 theta = x.angle(r)
 theta.style('V')
-theta.label("\u03B8",yoff=-5) #theta
+theta.label("\u03B8",yoff=-2) #theta
 
 M = r.extremite()
-M.label('M',yoff=-10)
+M.label('M',yoff=-5)
 er = gm.ligne(M.x, M.y, pi/6, 5)
 er.style('V')
 er.label("e_r",yoff=2)
